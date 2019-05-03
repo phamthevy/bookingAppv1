f0dbf476d0a15e527a78860911cf3dd91a75a128
DUC TU <rtntubmt97@gmail.com>
2019-05-03 18:50:56 +0700

    prepair for CartActivity

---
+++ app/src/main/java/com/bku/appbooking/common/InCartProduct.java
@@ -0,0 +1,15 @@
+package com.bku.appbooking.common;
+
+public class InCartProduct {
+    private Product product;
+    private int num;
+
+    public InCartProduct(Product product) {
+        this(product, 1);
+    }
+
+    public InCartProduct(Product product, int num) {
+        this.product = product;
+        this.num = num;
+    }
+}
