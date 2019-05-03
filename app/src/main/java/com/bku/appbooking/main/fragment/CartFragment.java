f4b36ea6c17ca132598252aab95af1a3de701d92
DUC TU <rtntubmt97@gmail.com>
2019-05-03 18:50:56 +0700

    prepair for CartActivity

--- app/src/main/java/com/bku/appbooking/main/fragment/CartFragment.java
+++ app/src/main/java/com/bku/appbooking/main/fragment/CartFragment.java
@@ -3,6 +3,7 @@ package com.bku.appbooking.main.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,9 +11,11 @@ import android.view.ViewGroup;
 import com.bku.appbooking.R;
 
 public class CartFragment extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.e(this.getClass().getName(), "onCreateView");
         return inflater.inflate(R.layout.fragment_cart, container, false);
     }
 }
---
+++ app/src/main/java/com/bku/appbooking/ultis/Cart.java
@@ -0,0 +1,29 @@
+package com.bku.appbooking.ultis;
+
+import com.bku.appbooking.common.InCartProduct;
+import com.bku.appbooking.common.Product;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Cart {
+    private static final Cart ourInstance = new Cart();
+    private List<InCartProduct> products;
+
+    public static Cart getInstance() {
+        return ourInstance;
+    }
+
+    private Cart() {
+        products = new ArrayList<>();
+    }
+
+    public void addProduct(InCartProduct product){
+        products.add(product);
+    }
+
+    public List<InCartProduct> getProducts(){
+        return products;
+    }
+}
