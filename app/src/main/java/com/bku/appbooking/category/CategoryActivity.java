6ea35c0797c416388c2c606be519ffd4d0a2d823
DUC TU <rtntubmt97@gmail.com>
2019-05-07 21:34:44 +0700

    add border to category image

--- app/src/main/java/com/bku/appbooking/category/CategoryActivity.java
+++ app/src/main/java/com/bku/appbooking/category/CategoryActivity.java
@@ -8,13 +8,17 @@ import android.widget.ImageView;
 
 import com.bku.appbooking.R;
 import com.bku.appbooking.common.Category;
+import com.bku.appbooking.ultis.CircleTransform;
 import com.google.gson.Gson;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
+
+import jp.wasabeef.picasso.transformations.RoundedCornersTransformation;
 
 
 public class CategoryActivity extends AppCompatActivity {
     private GridView gridView;
-    int categoryId;
+    private Category category;
     private int count = 0;
 
     @Override
@@ -22,18 +26,21 @@ public class CategoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
         Gson gson = new Gson();
-        Category category = gson.fromJson(getIntent().getStringExtra("category"), Category.class);
-        categoryId = category.getId();
+        category = gson.fromJson(getIntent().getStringExtra("category"), Category.class);
         gridView = findViewById(R.id.gridview);
+        ImageView imageView = findViewById(R.id.category_demo_img);
+        Transformation transformation = new RoundedCornersTransformation(20, 0);
+        Picasso.with(this).load(category.getImageUrl()).transform(transformation).into(imageView);
         setupGridview();
     }
 
     private void setupGridview(){
-        CategoryDataReceiver categoryDataReceiver = new CategoryDataReceiver(this, categoryId);
+        CategoryDataReceiver categoryDataReceiver = new CategoryDataReceiver(this, category.getId());
         final CategoryAdapter categoryAdapter = new CategoryAdapter(this, categoryDataReceiver);
         categoryAdapter.updateNextPage();
         gridView.setAdapter(categoryAdapter);
         gridView.setOnScrollListener(new AbsListView.OnScrollListener() {
+
             @Override
             public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
                 if (firstVisibleItem + visibleItemCount >= totalItemCount - 2) {
