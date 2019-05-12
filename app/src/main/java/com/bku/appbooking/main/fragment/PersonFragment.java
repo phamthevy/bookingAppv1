9afc8f4c0e5d1981d0b98480151f40433ad728d1
phamthevy <ptvy3197@gmail.com>
2019-05-12 19:25:24 +0700

    [UserInfo] Get Userinfo and auto fill

--- app/src/main/java/com/bku/appbooking/main/fragment/PersonFragment.java
+++ app/src/main/java/com/bku/appbooking/main/fragment/PersonFragment.java
@@ -1,9 +1,12 @@
 package com.bku.appbooking.main.fragment;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,8 +15,21 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.bku.appbooking.common.Product;
 import com.bku.appbooking.login.LoginActivity;
 import com.bku.appbooking.R;
+import com.bku.appbooking.ultis.CentralRequestQueue;
+import com.bku.appbooking.ultis.UserInfo;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class PersonFragment extends Fragment implements View.OnClickListener {
 
@@ -21,6 +37,7 @@ public class PersonFragment extends Fragment implements View.OnClickListener {
     Button btChangePerson, btCancel, btSaveChange, btChangePass, btLogout;
     public boolean isChangePass = false;
     LinearLayout layoutChangePass;
+    private CentralRequestQueue rQueue = CentralRequestQueue.getInstance();
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -28,6 +45,7 @@ public class PersonFragment extends Fragment implements View.OnClickListener {
         setUpView(view);
 
         //TODO: lay thong tin tu server de fill vao thong tin
+        getUserInfo();
         //fillInfo(view);
 
         btChangePerson.setOnClickListener(this);
@@ -39,6 +57,19 @@ public class PersonFragment extends Fragment implements View.OnClickListener {
         return view;
     }
 
+    private void getUserInfo(){
+        String url = "http://booking.vihey.com/api/getuserinfo.php";
+        requestGetUserInfo(url,UserInfo.getInstance().getAccessToken());
+        //TODO: sua lai thanh get phone
+        audoFillTextView(UserInfo.getInstance().getName(), UserInfo.getInstance().getMail(), UserInfo.getInstance().getAccessToken());
+    }
+
+    private void audoFillTextView(String name, String email, String phone){
+        txPersonName.setText(name);
+        txPersonEmail.setText(email);
+        txPersonPhone.setText(phone);
+    }
+
     @Override
     public void onClick(View v){
         switch(v.getId()){
@@ -46,15 +77,19 @@ public class PersonFragment extends Fragment implements View.OnClickListener {
                 setChangePersonInfo();
                 break;
             case R.id.btChangePass:
-                layoutChangePass.setVisibility(View.VISIBLE);
+//                layoutChangePass.setVisibility(View.VISIBLE);
+                new AlertDialog.Builder(getContext())
+                        .setMessage("Chưa thực hiện chức năng này")
+                        .setPositiveButton(R.string.yes, null)
+                        .show();
                 break;
             case R.id.btCancel:
                 setCancel();
                 break;
-            case R.id.btlogOut:
-                Intent intent = new Intent(getActivity(), LoginActivity.class);
-                startActivity(intent);
-                break;
+//            case R.id.btlogOut:
+//                Intent intent = new Intent(getActivity(), LoginActivity.class);
+//                startActivity(intent);
+//                break;
             case R.id.btSaveChange:
                 saveChange(
                         txPersonName.getText().toString(),
@@ -68,6 +103,8 @@ public class PersonFragment extends Fragment implements View.OnClickListener {
                 break;
         }
     }
+
+
 
     private void saveChange(String name, String phone, String email, String currentPass, String newPass, String confirmPass){
         //check null info
@@ -137,8 +174,54 @@ public class PersonFragment extends Fragment implements View.OnClickListener {
         btSaveChange = (Button) view.findViewById(R.id.btSaveChange);
         btChangePass = (Button) view.findViewById(R.id.btChangePass);
         btCancel = (Button) view.findViewById(R.id.btCancel);
-        btLogout = (Button)view.findViewById(R.id.btlogOut);
         layoutChangePass = (LinearLayout) view.findViewById(R.id.layoutChangePass);
+    }
+
+    private void requestGetUserInfo(final String url, final String accesstoken){
+        StringRequest MyStringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.e("Response: ", response);
+                try {
+                    response=new String(response.getBytes("ISO-8859-1"), "UTF-8");
+                    JSONObject object = new JSONObject(response);
+                    String message = object.optString("message");
+                    if ("Access granted".equals(message)){
+                        JSONObject objectData = new JSONObject(object.optString("data"));
+                        UserInfo.getInstance().setName(objectData.getString("hoten"));
+                        UserInfo.getInstance().setMail(objectData.getString("mail"));
+//                        UserInfo.getInstance().set(objectData.getString("diachi"));
+                    } else {
+                        new AlertDialog.Builder(getContext())
+                                .setMessage("Lấy thông tin user thất bại")
+                                .setPositiveButton(R.string.yes, null)
+                                .show();
+                    }
+
+                } catch (Exception e) {
+
+                }
+
+
+            }
+        }, new Response.ErrorListener() { //Create an error listener to handle errors appropriately.
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                new AlertDialog.Builder(getContext())
+                        .setMessage("Không thể lấy thông tin user")
+                        .setPositiveButton(R.string.yes, null)
+                        .show();
+            }
+        }) {
+            protected Map<String, String> getParams() {
+                Map<String, String> MyData = new HashMap<String, String>();
+                MyData.put("accesstoken", accesstoken);
+                return MyData;
+            }
+
+        };
+
+        rQueue.add(MyStringRequest);
     }
 
 }
